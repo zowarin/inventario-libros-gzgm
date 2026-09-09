47fff5d42dd32aee5bd17c1e08ebb9ba060f2627
zowar | tecniva <ivan.ramos@publitickets.com>
2026-09-09 13:09:35 -0600

    deploy github page

--- front-invetario-ng/src/app/app.config.ts
+++ front-invetario-ng/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes, withHashLocation())
   ]
 };
